48c1219376178951b419275e7da841b9e21c1992
keith(mac) <ka33145@googlemail.com>
2017-10-31 08:45:59 +0000

    Added test for adding to account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,4 +6,11 @@ describe Account do
       expect(subject.balance). to eq 0
     end
   end
+
+  describe '#add' do
+    it 'Add to balance' do
+      is_expected.to respond_to(:add).with(1).arguments
+      expect { subject.add(5) }.to change { subject.balance }.by 5
+    end
+  end
 end
